bdd363c88c01bae75755ab5a660cad5ade17fb34
Yousra <130630581+Yousra-Kamal@users.noreply.github.com>
2023-08-10 23:43:15 +1000

    updated header mobileNav/ article page intro

--- src/app/articles/page.jsx
+++ src/app/articles/page.jsx
@@ -43,20 +43,18 @@ async function Article({ article, publicationDomain, blogHandle }) {
 export default async function ArticlesIndex() {
   const data = await getArticleData();
 
-  // const posts = results.data.user.publication.posts;
-  // console.log("POSTS", posts);
   return (
     <>
       <Head>
         <title>Articles - Yousra Kamal</title>
         <meta
           name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+          content="My programming articles on Hashnode."
         />
       </Head>
       <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+        title="Writing on different Coding and Programming topics."
+        intro=" My programming articles on Hashnode."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
